dbe92188ce470a500ecbf12dddd4ac6c53d0dd4e
Vince Cima <contact@vincecima.com>
2024-02-25 19:31:59 -0600

    Shell for first commnad implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use clap::Command;
+const VERSION: Option<&str> = option_env!("CARGO_PKG_VERSION");
+const ABOUT: Option<&str> = option_env!("CARGO_PKG_DESCRIPTION");
+
 fn main() {
-    println!("Hello, world!");
+    let _matches = Command::new("fbin")
+        .version(VERSION.unwrap_or("unknown"))
+        .about(ABOUT.unwrap_or("unknown"))
+        .subcommand(Command::new("pages").subcommand(Command::new("create")))
+        .get_matches();
 }
